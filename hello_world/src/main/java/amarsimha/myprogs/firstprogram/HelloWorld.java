85b24be0a997242c759e0b4fd768dccbf1644265
amarsimha <amarsimha@gmail.com>
2016-02-10 08:19:16 -0800

    My First Commit - Helloworld.java

---
+++ hello_world/src/main/java/amarsimha/myprogs/firstprogram/HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package amarsimha.myprogs.firstprogram;
+
+/**
+ * @author amarsimha
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World. This is my first program on GIT.");
+
+	}
+
+}
